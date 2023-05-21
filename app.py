ba918fde4f74454a23f198b5e8a75aad1ad33f19
Zion Miller <djzionmiller@gmail.com>
2023-05-21 15:02:13 -0700

    adding async to run prompts concurrently

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from functools import partial
 import os
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from bson.json_util import dumps as mongo_dumps
+import asyncio
 import openai
-import logging
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
@@ -12,42 +14,35 @@ collection = db["summaries"]
 
 openai.api_key = os.environ.get('OPEN_API_KEY')
 
+async def fetch_summary(model, transcript, prompt, is_internal):
+    completion_prompt = prompt.format(transcript)
+    prompt_type = "internal" if is_internal else "external"
+    print(f"Starting fetch_summary for {prompt_type} prompt")
+    result = openai.ChatCompletion.create(
+        model=model,
+        messages=[{"role": "system", "content": completion_prompt}],
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    print(f"Completed fetch_summary for {prompt_type} prompt: {prompt}")
+    return result.choices[0].message.content
+
 @app.route("/summaries", methods=["POST"])
-def create_summary():
+async def create_summary():
     try:
         data = request.json
         call_transcript = data.get("transcript")
 
-        # Define the prompt for generating the combined summary
-        prompt = f"Generate two summaries for the following transcript and separate them into internal and external sections using ###SEPARATE### between the two sections. The internal summary should include the names on the call if present, key points, dates, dollar size, roadblocks, and any other relevant information. The external summary should include first-person language, use the customer name if it exists, a personalized thank you message, and provide a summary of key points from the discussion, including any questions answered. Outline the next steps and conclude with a final thank you.\n\n{call_transcript}"
+        internal_prompt = "Generate a summary for the following transcript, returning the following information, meant to be used as an internal summary for managers to read: names on the call if present, key points, dates, dollar size, roadblocks, and any other relevant information. Call transcript: {}"
+        external_prompt = "Generate a summary for the following transcript, returning the following information, meant to be used as an external summary for customers to read: include first-person language, use the customer name if it exists, a personalized thank you message, and provide a summary of key points from the discussion, including any questions answered. Call transcript: {}"
 
-        # Generate the combined summary using OpenAI API
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=1000,
-            temperature=0.7,
-            n=1,
-            stop=None,
-        )
+        internal_summary_task = asyncio.create_task(fetch_summary("gpt-3.5-turbo", call_transcript, internal_prompt, True))
+        external_summary_task = asyncio.create_task(fetch_summary("gpt-3.5-turbo", call_transcript, external_prompt, False))
 
-        summary = response.choices[0].text.strip()
+        internal_summary, external_summary = await asyncio.gather(internal_summary_task, external_summary_task)
 
-        # Find the starting index of the internal and external summaries
-        start_internal_summary = summary.find("Internal Summary:") + len("Internal Summary:")
-        start_external_summary = summary.find("External Summary:") + len("External Summary:")
-
-        # Extract the internal and external summaries
-        internal_summary = summary[start_internal_summary: start_external_summary].strip().replace('\n', '')
-        external_summary = summary[start_external_summary:].strip().replace('\n', '')
-
-        # Print or use the internal and external summaries as needed
-        print("Internal Summary:")
-        print(internal_summary)
-        print("\nExternal Summary:")
-        print(external_summary)
-
-        # Save the summaries in the database or perform any other desired actions
         summary_data = {
             "internal_summary": internal_summary,
             "external_summary": external_summary
@@ -63,11 +58,13 @@ def create_summary():
 @app.route("/summaries/<summary_id>", methods=["GET"])
 def get_summary(summary_id):
     try:
+        app.logger.info(f"getting summary for {summary_id}")
         # Convert summary_id to ObjectId
         obj_id = ObjectId(summary_id)
 
         # Find the summary document with the given ID
-        summary = collection.find_one({"_id": obj_id}, {"_id": 0})
+        summary = collection.find_one({"_id": obj_id})
+        summary["_id"] = str(summary["_id"])
 
         if summary:
             # Return the summary document as JSON response
@@ -76,6 +73,7 @@ def get_summary(summary_id):
             return jsonify({"message": "Summary not found"}), 404
 
     except Exception as e:
+        print(e)
         return jsonify({"message": "Error retrieving summary", "error": str(e)}), 500
 
 @app.route("/summaries/<summary_id>", methods=["DELETE"])
@@ -97,3 +95,4 @@ def delete_summary(summary_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
